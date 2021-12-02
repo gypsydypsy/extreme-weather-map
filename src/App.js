e4f2e33bee0309cedb2a980e08b22669209b8b19
Léa Marie <lea.marie3@live.fr>
2021-12-02 10:37:02 +0100

    mobile corrections

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './App.css'
 import Map from './components/Map'
 import { useGlobalContext } from './context'
 import Aside from './components/Aside'
+import { useEffect } from 'react'
 
 function App() {
 
   const { darkMode } = useGlobalContext()
+
+   //Corrects 100vh problems on mobile
+   useEffect ( () => {
+    const appHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--fullHeight', `${vh}px`);
+    }
+    window.addEventListener('resize', appHeight)
+    appHeight()
+  }, [])
 
   return (
     <div className={darkMode ?'container dark' : 'container'}>
